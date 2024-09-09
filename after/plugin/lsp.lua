dab11a275ae6d421d343576d9de6d9e2ac351b66
Yirun Zhou <yirunzhou@brandeis.edu>
2024-09-08 23:05:30 -0700

    ts ls and eslinter

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,42 +4,29 @@ local lsp_zero = require("lsp-zero")
 -- if there is a language server active in the file
 
 local lsp_attach = function(client, bufnr)
-  local opts = {buffer = bufnr}
+    local opts = { buffer = bufnr }
 
-  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-  vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 end
 
 lsp_zero.extend_lspconfig({
-  sign_text = true,
-  lsp_attach = lsp_attach,
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    sign_text = true,
+    lsp_attach = lsp_attach,
+    capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
 
 
 local lspconfig = require("lspconfig")
 local configs = require("lspconfig.configs")
-
--- ts js
-
-lspconfig.eslint.setup({
-    on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-        })
-    end,
-})
-
-require 'lspconfig'.eslint.setup {}
 
 -- yelp python server config
 -- https://yelp.slack.com/archives/CA7F2EM6G/p1673370381993289?thread_ts=1673342577.011049&cid=CA7F2EM6G
@@ -81,6 +68,10 @@ cmp.setup({
         -- `Enter` key to confirm completion
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
     }),
+    sources = {
+        { name = 'buffer' },
+        { name = 'nvim_lsp' },
+    },
 })
 
 -- mason
@@ -103,3 +94,26 @@ require("lspconfig").lua_ls.setup({
         },
     },
 })
+
+
+-- eslint
+lspconfig.eslint.setup({
+    root_dir = require('lspconfig').util.root_pattern(".eslintrc.json", "package.json", ".git"),
+    on_attach = function(client, bufnr)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            command = "EslintFixAll",
+        })
+    end,
+})
+
+-- require 'lspconfig'.eslint.setup {}
+
+require 'lspconfig'.ts_ls.setup {
+    on_attach = function(client, bufnr)
+        -- other custom settings
+    end,
+    flags = {
+        debounce_text_changes = 150,
+    }
+}
